8ab0dcc1a6c366f5f11702c776422eb8f83e7815
sai-pothuri <sai.messaldo@gmail.com>
2020-12-28 13:37:11 +0530

    added csv file

--- test.py
+++ test.py
@@ -10,4 +10,5 @@ import pandas as pd
 
 path="C:/Users/ADMIN/Documents/sai/data_science/chromedriver"
 df = gs.get_jobs('data scientist', 50, path, 20, False)
+
 df.to_csv('glassdoor_jpbs.csv', index = False)
